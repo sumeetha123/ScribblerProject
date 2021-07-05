710398ceda91fd72937d66e4c2019531bca85712
Sumeetha <sumeethakota@gmail.com>
2021-07-05 20:34:18 +0530

    added required comments for page

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 //functionality for CreatePosts button
 var Modal2 = document.getElementById("ModalC");
 var btn2 = document.getElementById("secondbutton");
-var span2 = document.getElementsByClassName("close")[2];
+var span2 = document.getElementsByClassName("close")[2];   //code to close 
 
 btn2.onclick = function () {
     Modal2.style.display = "block";
