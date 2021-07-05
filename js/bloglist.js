4fc37751b37a429f8379984520061f378c72083a
Sumeetha <sumeethakota@gmail.com>
2021-07-05 15:13:05 +0530

    Added functionality for delete button

---
+++ js/bloglist.js
@@ -0,0 +1,38 @@
+//functionality for Sign Up Button
+var DelModal = document.getElementById("blog-postmodal");
+var DelModal1 = document.getElementById("blog-postmodal1");
+var DelModal2 = document.getElementById("blog-postmodal2");
+var DelModal3 = document.getElementById("blog-postmodal3");
+var DelModal4 = document.getElementById("blog-postmodal4");
+
+
+var delbtn = document.getElementById("delete-btn");
+var delbtn1 = document.getElementById("delete-btn1");
+var delbtn2 = document.getElementById("delete-btn2");
+var delbtn3 = document.getElementById("delete-btn3");
+var delbtn4 = document.getElementById("delete-btn4");
+
+
+delbtn.onclick = function () {
+    DelModal.style.display = "block";
+}
+
+delbtn1.onclick = function () {
+    DelModal1.style.display = "block";
+}
+
+delbtn2.onclick = function () {
+    DelModal2.style.display = "block";
+}
+
+delbtn3.onclick = function () {
+    DelModal3.style.display = "block";
+}
+
+delbtn4.onclick = function () {
+    DelModal4.style.display = "block";
+}
+
+
+
+
